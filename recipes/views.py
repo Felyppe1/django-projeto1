8075d629e8403595a53254872573de24b492993c
Luiz Felyppe <felyppe.nunes1@gmail.com>
2023-05-12 20:43:10 -0300

    aula 62

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(
@@ -16,8 +17,13 @@ def category(request, category_id):
         category__id=category_id, #__ serve para acessar campos da foreign key
         is_published=True  
     ).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found')
+
     return render(request, 'recipes/pages/category.html', {
-        'recipes': recipes #[make_recipe() for _ in range(10)]
+        'recipes': recipes, #[make_recipe() for _ in range(10)]
+        'title': f'{recipes.first().category.name} - Category | '
     }
 )
 
