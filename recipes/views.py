5c4a2d4b410bb3cd02c5e241d0d327fe99e0cb8e
Luiz Felyppe <felyppe.nunes1@gmail.com>
2023-05-12 19:48:46 -0300

    aula 58

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', {
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes #[make_recipe() for _ in range(10)]
+    }
+)
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id  #__ serve para acessar campos da foreign key
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', {
+        'recipes': recipes #[make_recipe() for _ in range(10)]
     }
 )
 
